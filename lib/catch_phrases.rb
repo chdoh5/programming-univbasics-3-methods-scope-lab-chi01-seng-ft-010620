a5c9871419639b936d32f83f078699aacbc7eb43
Christine Doherty <christineadoherty@gmail.com>
2019-11-03 21:19:48 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -8,3 +8,8 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link 
+  phrase_link = "It's Dangerous To Go Alone! Take This."
+  puts phrase_link
+end
